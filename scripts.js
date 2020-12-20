61434f90a5a500d773e8e962b1c3710465e084c5
Won Yang <won@gatech.edu>
2020-12-19 19:19:52 -0600

    added submit box without functionarlity

--- scripts.js
+++ scripts.js
@@ -1,37 +1,44 @@
-console.log(axios);
-
-const request = new XMLHttpRequest();
+// URL Endpoints
 const baseUrl = "https://api.openopus.org/";
 const essentialComposers = baseUrl + "composer/list/rec.json";
 
+// Selecting divs
 const divOfPiece = document.getElementById("pieceName");
 const divOfComposer = document.getElementById("composerName");
 
-axios.get(essentialComposers)
-	.then(data => {
-		const composers = data["data"]["composers"];
-		console.log(composers);
-		const num = Math.floor((Math.random() * 76));
-		const composerID = composers[num]["id"];
-		const composerName = composers[num]["name"];
-		divOfComposer.textContent = composerName;
-		console.log(composerID);
-		getPiece(composerID);
-	})
-	.catch(err => console.log(err));
+// Initialize
+getComposer();
+listenForSubmit();
+
+function getComposer() {
+	axios.get(essentialComposers)
+		.then(data => {
+			const composers = data["data"]["composers"];
+			const num = Math.floor((Math.random() * 77));
+			const composerID = composers[num]["id"];
+			const composerName = composers[num]["name"];
+			divOfComposer.textContent = "Composer: " + composerName;
+			getPiece(composerID);
+		})
+		.catch(err => console.log(err));
+}
 
 function getPiece(id) {
 	const pieceURL = baseUrl + "work/list/composer/" + id + "/genre/all.json";
 	axios.get(pieceURL)
 		.then((data) => {
-			console.log(data);
 			const composerWorks = data["data"]["works"];
-			const numOfWorks = composerWorks.length - 1;
+			const numOfWorks = composerWorks.length;
 			const numOfWork = Math.floor((Math.random() * numOfWorks));
-			console.log(numOfWork);
 			const title = composerWorks[numOfWork]["title"];
-			console.log(title);
-			divOfPiece.textContent = title;
+			divOfPiece.textContent = "Piece: " + title;
 		})
 		.catch(err => console.log(err));
+};
+
+function listenForSubmit() {
+	const submitButton = document.getElementById("submit");
+	submitButton.addEventListener("click", () => {
+		console.log("hi");
+	});
 };
